f3b54e7478cddd9d1854dd7009b643e4d0eec29c
Vasli Saidmuradov <vasli.said@gmail.com>
2022-01-11 00:02:32 +0600

    feat: add Singly Linked List DS

---
+++ section-19-Singly Linked Lists/singlyLinkedList.js
@@ -0,0 +1,188 @@
+class Node {
+  constructor (val) {
+    this.val = val
+    this.next = null
+  }
+}
+
+class SinglyLinkedList {
+  constructor () {
+    this.head = null
+    this.tail = null
+    this.length = 0
+  }
+
+  push (val) {
+    const newNode = new Node(val)
+
+    if (!this.head) {
+      this.head = newNode
+      this.tail = this.head
+    } else {
+      this.tail.next = newNode
+      this.tail = newNode
+    }
+
+    this.length++
+
+    return this
+  }
+
+  pop () {
+    if (!this.head) return undefined
+
+    let current = this.head
+    let newTail = current
+    
+    while (current.next) {
+      newTail = current
+      current = current.next
+    }
+
+    this.tail = newTail
+    this.tail.next = null
+    this.length--
+
+    if (!this.length) {
+      this.head = null
+      this.tail = null
+    }
+
+    return current
+  }
+
+  shift () {
+    if (!this.head) return undefined
+
+    let currentHead = this.head
+    this.head = currentHead.next
+    this.length--
+
+    if (!this.length) {
+      this.tail = null
+    }
+
+    currentHead.next = null
+
+    return currentHead
+  }
+
+  unshift (val) {
+    const newNode = new Node(val)
+
+    if (!this.head) {
+      this.head = newNode
+      this.tail = newNode
+    } else {
+      newNode.next = this.head
+      this.head = newNode
+    }
+
+    this.length++
+    return this
+  }
+
+  get (idx) {
+    if (idx < 0 || idx >= this.length) return null
+    
+    let counter = 0
+
+    let list = this.head
+
+    while (counter < idx) {
+      list = list.next
+      counter++
+    }
+
+    return list
+  }
+
+  set (idx, val) {
+    const targetNode = this.get(idx)
+
+    if (targetNode) {
+      targetNode.val = val
+
+      return true
+    }
+
+    return false
+  }
+
+  insert (idx, val) {
+    if (idx < 0 || idx > this.length) return false
+    if (idx === 0) return !!this.unshift(val)
+    if (idx === this.length) return !!this.push(val)
+
+    const prevNode = this.get(idx - 1)
+    const newNode = new Node(val)
+    const temp = prevNode.next
+
+    prevNode.next = newNode
+    newNode.next = temp
+
+    this.length++
+
+    return true
+  }
+
+  remove (idx) {
+    const targetNode = this.get(idx)
+
+    if (!targetNode) return false
+    if (idx === 0) return this.shift()
+    if (idx === this.length - 1) return this.pop()
+
+    const prevNode = this.get(idx - 1)
+    prevNode.next = targetNode.next
+    
+    this.length--
+
+    targetNode.next = null
+    
+    return targetNode
+  }
+}
+
+// HI -> 99 -> 100
+
+const list = new SinglyLinkedList()
+list.push('Hi')
+list.push('99')
+list.push('100')
+
+// console.log(list.unshift(100))
+// console.log(list.unshift(200))
+// console.log(list.unshift(300))
+// console.log(list.shift())
+
+// console.log(list.push('110'))
+// console.log(list.push('120'))
+// console.log(list.pop())
+// console.log(list.pop())
+// console.log(list.pop())
+
+
+// console.log(list.shift())
+// console.log(list.shift())
+
+// console.log(list.get(-1))
+// console.log(list.get(0))
+// console.log(list.get(1))
+// console.log(list.get(2))
+// console.log(list.get(3))
+
+// console.log(list.set(0, 'Lets go!'))
+// console.log(list.set(1, 'Hello'))
+// console.log(list.set(2, 'world'))
+// console.log(list.set(3, 'no node!!!'))
+
+// console.log(list.insert(0, 'First'))
+// console.log(list.insert(3, 'Last'))
+// console.log(list.insert(2, '$$$'))
+
+// console.log(list.remove(0))
+// console.log(list.remove(1))
+// console.log(list.remove(2))
+
+console.log(list)
